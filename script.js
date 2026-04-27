4c91b6ff6e2ba3344e563497c01934226ab64704
Md Nouman <noumantannu06@gmail.com>
2026-04-27 16:13:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-//your JS code here. If required.
 const ip = document.getElementById("ip");
-const btn = document.getElementByid("btn");
+const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
-function delayedValue (value, time) {
-	return new promise ((resolve) => {
-		setTimeout(() => resolve(value). time);
-	});
+function delayedValue(value, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, time);
+  });
 }
 
-btn.onelick = function () {
-	output.innerText = "";
+btn.addEventListener("click", () => {
+  output.innerText = "";
+  const num = Number(ip.value);
 
-	const num = Number(ip.value);
-
-	delayedValue(num, 2000)
-	.then((result) => {
-		output.innerText = `Result: ${result}`;
-		return delayedValue(result * 2, 2000);
-	})
-	.then((result) =>{
-		output.innerText = `Result: ${result}`;
-		 return delayedValue(result - 3, 1000);
-	})
-	.then((result) => {
-		output.innerText = `Result: ${result}`;
-		return delayedValue(result / 2, 1000);
-	})
-	.then((result) => {
-		output.innerText = `Result: ${result}`;
-		return delayedValue(result + 10, 1000);
-	})
-	.then((result) => {
-		output.innerText = `Final Result: ${result}`;
-	});
-	
-};
+  delayedValue(num, 2000)
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedValue(result * 2, 2000);
+    })
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedValue(result - 3, 1000);
+    })
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedValue(result / 2, 1000);
+    })
+    .then((result) => {
+      output.innerText = `Result: ${result}`;
+      return delayedValue(result + 10, 1000);
+    })
+    .then((result) => {
+      output.innerText = `Final Result: ${result}`;
+    });
+});
